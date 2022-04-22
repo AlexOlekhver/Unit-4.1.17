c3148199d81ff97ebbdc5a67913495040a1fc209
Leshich <leshicher@mail.ru>
2022-04-22 16:19:16 +0300

    Add program "switch"

---
+++ Project Switch/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Project_Switch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Напишите свой любимый цвет на английском маленькими буквами");
+            
+            var color = Console.ReadLine();
+            
+            switch (color)
+            {
+                case "red":
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.WriteLine("your favorite color red!");
+                    break;
+
+                case "green":
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.WriteLine("your favorite color green!");
+                    break;
+
+                case default:
+                    Console.BackgroundColor = ConsoleColor.Cyan;
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("your favorite color cyan!");
+                    break;
+            }
+        }
+    }
+}
