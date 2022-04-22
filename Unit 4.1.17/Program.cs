24781a2b0364f3d9f0ff6a3c981fedf7951e05a6
Leshich <leshicher@mail.ru>
2022-04-22 06:25:31 +0300

    Add project files.

---
+++ Unit 4.1.17/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Unit_4._1._17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
