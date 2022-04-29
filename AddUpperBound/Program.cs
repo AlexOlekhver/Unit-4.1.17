6c8b3da8a4aa9dfedfb75e553200752b2499aef1
Leshich <leshicher@mail.ru>
2022-04-29 07:01:54 +0300

    add operator FOR to write table

--- AddUpperBound/Program.cs
+++ AddUpperBound/Program.cs
@@ -7,11 +7,13 @@ namespace AddUpperBound
         static void Main(string[] args)
         {
             int[,] array = { { 1, 2, 3 }, { 5, 6, 7 } };
-            Console.Write("Количество строк: ");
-            Console.WriteLine(array.GetUpperBound(0)+1);
+            for (int i = 0; i < array.GetUpperBound(0) + 1; i++)
+            {
+                for (int k = 0; k < array.GetUpperBound(1) + 1; k++)
+                    Console.Write(array[i , k] + " ");
 
-            Console.Write("Количество колонок: ");
-            Console.WriteLine(array.GetUpperBound(1) + 1);
+                Console.WriteLine();
+            }
         }
     }
 }
