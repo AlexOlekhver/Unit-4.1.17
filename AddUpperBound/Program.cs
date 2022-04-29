5fcd10fd766842cbf964946c9021316e2982f576
Leshich <leshicher@mail.ru>
2022-04-29 06:51:37 +0300

    add operator getupperbound

--- AddUpperBound/Program.cs
+++ AddUpperBound/Program.cs
@@ -7,7 +7,11 @@ namespace AddUpperBound
         static void Main(string[] args)
         {
             int[,] array = { { 1, 2, 3 }, { 5, 6, 7 } };
-            Console.WriteLine(array.Length);
+            Console.Write("Количество строк: ");
+            Console.WriteLine(array.GetUpperBound(0)+1);
+
+            Console.Write("Количество колонок: ");
+            Console.WriteLine(array.GetUpperBound(1) + 1);
         }
     }
 }
