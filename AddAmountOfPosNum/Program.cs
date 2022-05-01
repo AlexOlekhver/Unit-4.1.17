a8dd02f5ad49d87dfd23310bc3242221ec7e6524
Leshich <leshicher@mail.ru>
2022-05-01 13:36:22 +0300

    AddAmountOfPosNum

---
+++ AddAmountOfPosNum/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace AddAmountOfPosNum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = new int[] {5, 6, -3, 6, -3, 6, -3, 6, -3, 5};
+            var numbers = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] > 0)
+                {
+                    numbers++;
+                }
+            }
+            Console.WriteLine(numbers);
+            Console.ReadKey();
+        }
+    }
+}
