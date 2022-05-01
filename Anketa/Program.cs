c8e5883b1db13d255d1e4912729ce516d8bf6aa7
Leshich <leshicher@mail.ru>
2022-05-01 17:10:09 +0300

    Add anketa

---
+++ Anketa/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Anketa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            (string name, int age) anketa;
+            Console.WriteLine("Введите свое имя: ");
+            anketa.name = Console.ReadLine();
+            Console.Write("Введите возраст цифрами: ");
+            anketa.age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Ваше имя: {0}", anketa.name);
+            Console.WriteLine("Ваш возраст: {0}", anketa.age);
+            Console.ReadKey();
+        }
+    }
+}
