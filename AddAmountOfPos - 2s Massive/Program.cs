798ca5ba2e777443000b953bfd8c57773a0a8d05
Leshich <leshicher@mail.ru>
2022-05-01 16:42:03 +0300

    AddAmountOfPos - 2s Massive

---
+++ AddAmountOfPos - 2s Massive/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace AddAmountOfPos___2s_Massive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] arr = { { -5, 6, -9, -1, 2, -3}, {-8, 8, 1, 1, 2, -3 } };
+            var numbers = 0;
+            
+            for (int i = 0; i < arr.GetUpperBound(0) + 1; i++)
+            {
+                for (int j = 0; j < arr.GetUpperBound(1) + 1; j++)
+                {
+                    if (arr[i, j] > 0)
+                    {
+                    numbers++;
+                    }
+                }
+            }
+            Console.WriteLine(numbers);
+            Console.ReadKey();
+        }
+    }
+}
