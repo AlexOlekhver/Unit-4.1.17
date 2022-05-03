f0b069042c72145e3acacf5fd9935e34fb05234e
Leshich <leshicher@mail.ru>
2022-05-03 18:35:53 +0300

    Add 5.1.6. - Get Array

---
+++ Module 5.1.6/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Module_5._1._6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetArrayFromConsole();
+        }
+    }
+    static int[] GetArrayFromConsole()
+    {
+        var result = new int[5];
+
+        for (int i=0; i < result.Length; i++)
+        {
+            Console.WriteLine("Please insert element of massive no {0}", i + 1);
+            result[i] = Convert.ToInt32(Console.ReadLine());
+        }
+        return result;
+    }
+
+}
