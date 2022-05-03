3e5cfcf26f52981f89a0f7a27cc17b34fb442d0a
Leshich <leshicher@mail.ru>
2022-05-03 12:20:39 +0300

    add Module5 - User

---
+++ Module 5 - User/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Module_5___User
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            (string name, string[] dishes) User;
+            Console.WriteLine("Print your mane : ");
+            User.name = Console.ReadLine();
+
+            User.dishes = new string[5];
+            for (int i =0; i < User.dishes.Length;  i++ )
+            {
+                Console.WriteLine("Please insert your favorite dish {0}: ", i+1);
+                User.dishes[i] = Console.ReadLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
