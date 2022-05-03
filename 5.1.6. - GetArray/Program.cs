562c1e392b4ef3537ad96480416b0ccb1702c40b
Leshich <leshicher@mail.ru>
2022-05-03 19:03:03 +0300

    Add 5.1.6. GettArray

--- 5.1.6. - GetArray/Program.cs
+++ 5.1.6. - GetArray/Program.cs
@@ -8,15 +8,33 @@ namespace _5._1._6.___GetArray
         {
             GetArrayFromConsole();
         }
-    
+
         static int[] GetArrayFromConsole()
         {
             var result = new int[5];
 
             for (int i = 0; i < result.Length; i++)
-           {
-            Console.WriteLine("Please insert element of massive no {0}", i + 1);
-            result[i] = Convert.ToInt32(Console.ReadLine());
+            {
+                Console.WriteLine("Please insert element of massive no {0}", i + 1);
+                result[i] = Convert.ToInt32(Console.ReadLine());
+            }
+           
+            int temp = 0;
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                for (int j = i + 1; j < result.Length; j++)
+                    if (result[i] < result[j])
+                    {
+                        temp = result[i];
+                        result[i] = result[j];
+                        result[j] = temp;
+                    }
+            }
+
+            foreach (var item in result)
+            {
+                Console.Write(item);
             }
             return result;
         }
