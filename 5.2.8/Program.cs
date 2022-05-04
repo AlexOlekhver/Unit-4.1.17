dd459f363eaa83753f49e3db18505c99c3b67876
Leshich <leshicher@mail.ru>
2022-05-04 06:21:55 +0300

    Add 5.2.8

---
+++ 5.2.8/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _5._2._8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetArrayFromConsole();
+        }
+
+        static int[] GetArrayFromConsole()
+        {
+            var result = new int[5];
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                Console.WriteLine("Please insert element of massive no {0}", i + 1);
+                result[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            return result;
+        }
+        static int[] SortArray(int[] array)
+        {
+            array = GetArrayFromConsole();
+
+            int temp = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = i + 1; j < array.Length; j++)
+                    if (array[i] < array[j])
+                    {
+                        temp = array[i];
+                        array[i] = array[j];
+                        array[j] = temp;
+                    }
+            }
+            return array;
+        }
+
+    }
+}
+
--- Module 5.1 ShowColor - Massive/Program.cs
+++ Module 5.1 ShowColor - Massive/Program.cs
@@ -6,9 +6,9 @@ namespace Module_5._1_ShowColor___Massive
     {
         static void Main(string[] args)
         {
-            static string ShowColor(string username)
+            static string ShowColor(string username, int userage)
             {
-                Console.WriteLine("{0} ,Напишите свой любимый цвет на английском с маленькой буквы", username);
+                Console.WriteLine("{0}, {1} years old \n Напишите свой любимый цвет на английском с маленькой буквы", username, userage);
                 var color = Console.ReadLine();
 
                 switch (color)
@@ -58,7 +58,7 @@ namespace Module_5._1_ShowColor___Massive
             var favColors = new string[3];
             for (int i = 0; i < favColors.Length ; i++)
                 {
-                    favColors[i] = ShowColor(name);
+                    favColors[i] = ShowColor(name , age);
                 }
 
             Console.WriteLine("Yours favorite colors are: ");
