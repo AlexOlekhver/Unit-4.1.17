26a8435db07aa119b12001c47186e5f1e1731bac
Leshich <leshicher@mail.ru>
2022-05-06 23:55:51 +0300

    in 5.5.5 change type to decimal

---
+++ Module 5.5.5 - Factorial/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Module_5._5._5___Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение икс:");
+            int x = int.Parse(Console.ReadLine()); 
+            Factorial(x);
+            Console.WriteLine(Factorial(x));
+        }
+
+        static decimal Factorial(int x)
+        {
+            if (x == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return x * Factorial(x - 1);
+            }
+        }
+    }
+}
